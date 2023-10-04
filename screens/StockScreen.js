d44ddcc1274c281386170efbc6067a5f63bca2eb
djiby thioub <thioubdjiby96@gmail.com>
2023-10-04 18:25:37 +0000

    update the storeScreen

--- screens/StockScreen.js
+++ screens/StockScreen.js
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { CardStyleInterpolators } from '@react-navigation/stack'
 
 const StockScreen = () => {
   return (
@@ -11,4 +12,9 @@ const StockScreen = () => {
 
 export default StockScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    alignItems:center
+  }
+})
