0a34949eafd0114fe7830dcdf72fe62ea2c468f7
Mahammedi Abdelghani <wboductqr@relay.firefox.com>
2024-06-22 13:34:28 +0100

    ajout: animation des different moyens de paiement
    - run npm i to install `@react-native-picker/picker`

--- screens/PaymentScreen.js
+++ screens/PaymentScreen.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
+import PaymentModal from "../components/PaymentModal";
 
 const PaymentScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity onPress={() => navigation.goBack()} style={styles.backButton}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={styles.backButton}
+        >
           <Image
-            source={require('../assets/icons/flecheretour.jpg')}
+            source={require("../assets/icons/flecheretour.jpg")}
             style={styles.backIcon}
           />
         </TouchableOpacity>
@@ -30,29 +34,27 @@ const PaymentScreen = ({ navigation }) => {
           encore plus pratiques et sécurisées.
         </Text>
       </View>
-      <TouchableOpacity style={styles.button}>
-        <Text style={styles.buttonText}>Ajouter une méthode de paiement</Text>
-      </TouchableOpacity>
+        <PaymentModal />
     </View>
   );
-}
+};
 
 export default PaymentScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 16,
   },
   header: {
-    position: 'absolute',
+    position: "absolute",
     top: 40,
     left: 16,
   },
   backButton: {
     padding: 8,
-    backgroundColor: '#F0F0F0',
+    backgroundColor: "#F0F0F0",
     borderRadius: 25,
   },
   backIcon: {
@@ -61,38 +63,38 @@ const styles = StyleSheet.create({
   },
   topSection: {
     marginTop: 80, // Ajustez cette valeur pour positionner les textes en haut
-    alignItems: 'center', // Centre les éléments enfants horizontalement
-    width: '100%', // Assure que le texte prend toute la largeur disponible
+    alignItems: "center", // Centre les éléments enfants horizontalement
+    width: "100%", // Assure que le texte prend toute la largeur disponible
     paddingHorizontal: 16, // Ajoute du padding pour l'alignement à gauche
-    textAlign: 'left', // Alignement à gauche
+    textAlign: "left", // Alignement à gauche
   },
   title: {
     fontSize: 24, // Taille de la police
-    color: '#4CAF50',
+    color: "#4CAF50",
     marginBottom: 20,
-    textAlign: 'center', // Alignement à gauche
+    textAlign: "center", // Alignement à gauche
   },
   description: {
     fontSize: 14, // Taille de la police
-    color: 'rgba(140, 140, 140, 0.5)', // Couleur gris clair avec opacité
-    textAlign: 'left', // Alignement à gauche
+    color: "rgba(140, 140, 140, 0.5)", // Couleur gris clair avec opacité
+    textAlign: "left", // Alignement à gauche
     lineHeight: 22, // Hauteur de ligne pour un meilleur espacement
     marginBottom: 5, // Ajoute de la marge pour espacer les lignes
   },
   button: {
-    backgroundColor: '#4CAF50',
+    backgroundColor: "#4CAF50",
     paddingVertical: 12,
     paddingHorizontal: 24,
     borderRadius: 10, // Rendre les coins du bouton arrondis
-    alignItems: 'center',
+    alignItems: "center",
     marginTop: 50, // Espace entre la description et le bouton
-    alignSelf: 'center', // Centrer le bouton horizontalement
+    alignSelf: "center", // Centrer le bouton horizontalement
   },
   buttonText: {
-    color: 'black',
+    color: "black",
     fontSize: 16,
     width: 250,
     height: 25,
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
